f6e3ab242a6fdbad2d4f7a687524727d6d60ffe3
priyanshu239 <priyanshuguptaablack@gmail.com>
2026-04-24 11:49:37 +0530

    setup redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import NavBar from "./NavBar"
+import Body from "../src/components/Body";
+import Login from "../src/components/Login";
+import Profile from "../src/components/Profile";
+import Feed from "../src/components/Feed";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
-
   return (
-    <>    
-      <NavBar/>
-      <h1 className='text-3xl font-bold'>Hello World</h1>
-          
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+  const [emailId, setEmailId] = useState("qwerty@gmail.com");
+  const [password, setPassword] = useState("Abcd@1234");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true },
+      );
+
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center py-20 ">
+      <div className="card bg-base-300 w-96 shadow-sm max-h-xs">
+        <div className="card-body">
+          <h2 className="card-title justify-center ">Login</h2>
+          <div className="flex flex-col gap-4">
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Email ID</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+                value={emailId}
+                onChange={(e) => setEmailId(e.target.value)}
+              />
+            </label>
+
+            <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                className="input input-bordered w-full max-w-xs"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center mt-4">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
